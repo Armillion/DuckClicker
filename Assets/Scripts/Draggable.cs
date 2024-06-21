0b7f8ee3b570f8c6f98b324ecc670def59492e68
Armillion <96535145+Armillion@users.noreply.github.com>
2024-06-21 19:11:14 +0200

    drag and drop (wip)

--- Assets/Scripts/Draggable.cs
+++ Assets/Scripts/Draggable.cs
@@ -4,5 +4,23 @@ using UnityEngine;
 
 public class Draggable : MonoBehaviour
 {
-    
+    private Transform parent;
+    private Vector3 pos;
+
+    private void Start()
+    {
+        parent = transform.parent;
+        pos = transform.localPosition;
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        //Chceck if collision is with puzzle and act appropiatly
+    }
+
+    public void stop()
+    {
+        transform.position = pos;
+        transform.parent = parent;
+    }
 }
--- Assets/Scripts/DraggingController.cs
+++ Assets/Scripts/DraggingController.cs
@@ -4,15 +4,79 @@ using UnityEngine;
 
 public class DraggingController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
+    private bool isDragging = false;
+
+    private Vector2 screenPos;
+    private Vector3 worldPos;
         
+    private Draggable lastDragged;
+
+    private void Awake()
+    {
+        DraggingController[] controllers = FindObjectsOfType<DraggingController>();
+        if (controllers.Length > 1)
+            Destroy(this);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (isDragging && (Input.GetMouseButtonUp(0) || (Input.touchCount == 1 && Input.GetTouch(0).phase == TouchPhase.Ended)))
+        {
+            drop();
+            return;
+        }
+
+        if(Input.GetMouseButton(0))
+        {
+            Vector3 mousePos = Input.mousePosition;
+            screenPos = new Vector2(mousePos.x, mousePos.y);
+        }
+        else if(Input.touchCount > 0)
+        {
+            screenPos = Input.GetTouch(0).position;
+        }
+        else
+        {
+            return;
+        }
+
+        worldPos = Camera.main.ScreenToWorldPoint(screenPos);
+
+        if(isDragging)
+        {
+            Drag();
+        }
+        else
+        {
+            RaycastHit2D hit = Physics2D.Raycast(worldPos, Vector2.zero);
+            
+            if(hit.collider != null)
+            {
+                Debug.Log(hit.transform.gameObject.name);
+                Draggable draggable = hit.transform.gameObject.GetComponent<Draggable>();
+                if(draggable != null)
+                {
+                    lastDragged = draggable;
+                    hit.transform.parent = null;
+                    initDrag();
+                }
+            }
+        }
+    }
+
+    private void initDrag()
+    {
+        isDragging = true;
+    }
+
+    private void Drag()
+    {
+        lastDragged.transform.position = new Vector2(worldPos.z, worldPos.y);
+    }
+
+    private void drop()
+    {
+        isDragging = false;
+        lastDragged.stop();
     }
 }
